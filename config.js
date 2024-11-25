1ca61defd06f7841afd6eaf69c8bd32cb435e5d0
Anshul Gupta <89581549+Anshul-2806@users.noreply.github.com>
2024-11-25 19:02:53 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+const CONFIG = {
+    titleWeb: "Best wishes for you",
+    introTitle: 'Hello my bestie!, Beautiful kudi & congrats for starting new journey from April, 2k24 !',
+    introDesc: `Congratulations on your wedding my dear friend.
+Happy Married Life my friend. May you both fall in love again and again.
+I hope you have the most amazing time as you say those vows my dearest friend. Congratulations.
+Happy Marriage to both of you! I wish you all the best for your life’s journey.
+Finally, two souls become one and two hearts beat as one. ...`,
+    btnIntro: 'click me',
+    title: 'Happy Married Life my friend. May you both fall in love again and again 🥰🥰',
+    desc: 'Happy Marriage to both of you! I wish you all the best for your new life’s journey',
+    btnYes: 'Congrats! You',
+    btnNo: 'cancel, did not accept..',
+    question: 'There are more than 7 billion people in the world, why do you give best wishes you',
+    btnReply: 'Sent to you ',
+    reply: 'Best friends are difficult to come by in this world. They are like precious gifts bestowed on us by the universe',
+    mess: 'Congrats again 🥰, best wishes to you 😘',
+    // messDesc: 'Tonight at 7pm, I will come pick you up to go out..',
+    btnAccept: 'Okayy',
+    messLink: 'https://github.com/anshul-2806/' 
+}
